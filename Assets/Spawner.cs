79830a89353a91221ef8f47f186704d2c3e015ac
Chuhan Feng <simonrouse9461@gmail.com>
2016-10-01 15:46:39 -0400

    initial

---
+++ Assets/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject SmallSphere;
+
+	// Use this for initialization
+	void Start () {
+		var pos = transform.position;
+		var sphere = Instantiate(SmallSphere, new Vector3(pos.x, pos.y + 2, pos.z), Quaternion.identity);
+		sphere.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

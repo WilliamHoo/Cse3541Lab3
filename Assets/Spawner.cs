e769439d707bcd5a6a79cd630bf8338bcf94ba7a
WilliamHoo <william.haoqi@gmail.com>
2016-10-01 16:29:47 -0400

    three small spheres

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -9,8 +9,17 @@ public class Spawner : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		var pos = transform.position;
-		var sphere = Instantiate(SmallSphere, new Vector3(pos.x, pos.y + 2, pos.z), Quaternion.identity);
-		sphere.SetActive(true);
+
+		var spheres = new GameObject[3];
+
+		var pos_adjust = 0;
+
+		for (int i = 0; i < spheres.Length; i++) {
+			spheres[i]  = Instantiate(SmallSphere, new Vector3(pos.x + i, pos.y + 2, pos.z), Quaternion.identity);
+			spheres[i].SetActive(true);
+		}
+			
+
 	}
 	
 	// Update is called once per frame
